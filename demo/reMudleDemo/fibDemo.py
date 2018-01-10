67d03831bc2db62c409e1fecb6814cedbc1e0e9a
guduxuespn <guduxue@hotmail.com>
2018-01-10 17:27:44 +0800

    完善了fib函数

--- demo/reMudleDemo/fibDemo.py
+++ demo/reMudleDemo/fibDemo.py
@@ -1,14 +1,12 @@
+# 该函数可以输出n以内的斐波那契数列
+# 
+
 def fib(n):
     a = 0
     b = 1
-    a,b=a,a+b
-    print (a,b)
-    c = a + b
-    if c < n:
-        for m in range(0, n):
-            c = a + b
-            a = b
-            print(a, c)
+    while b < n:
+        print(a)
+        a, b = b, a + b
 
 
 fib(20)
